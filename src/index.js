e20a04b837cae0beac7b04cbcd054b729405486d
FangFeiyue <18766131540@163.com>
2017-10-31 19:01:30 +0800

    redux最基本的使用

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+ import { createStore } from 'redux';
+ 
+ function counter(state=0, action){
+    switch(action.type){
+        case 'INCREASE':
+            return state + 1;
+        case 'DECREASE':
+            return state - 1;
+        default:
+            return state;
+    }
+ }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ function listener(){
+    //  获取state
+     const currState = store.getState();
+     console.log(`现在的值为${currState}`);   
+}
+
+//  新建store
+ const store = createStore(counter);
+
+ store.subscribe(listener);
+
+//  派发事件，传递action
+ store.dispatch({
+     type: 'INCREASE'
+ });
+ 
