ed2f0ed19be7002f4f8612d9c4c1348e32728490
FangFeiyue <18766131540@163.com>
2017-11-03 09:32:00 +0800

    建立登录注册数据库模型

--- project/server/model.js
+++ project/server/model.js
@@ -3,3 +3,33 @@ const mongoose = require('mongoose');
 // 连接mongo
 const DB_URL = 'mongodb://127.0.0.1:27017/test_mongo';
 mongoose.connect(DB_URL);
+
+const models = {
+    user:{
+        'user'   : {type: String, require: true},
+        'pwd'    : {type: String, require: true},
+        'type'   : {type: String, require: true},
+        // 头像
+        'avatar' : {type: String},
+        // 个人简介或职位描述
+        'desc'   : {type: String},
+        // 职位名
+        'title'  : {type: String},
+        // 如果是boss还有company和money两个字段
+        'company': {type: String},
+        'money'  : {type: String}
+    },
+    chat:{
+
+    }
+};
+
+for (let m in models){
+    mongoose.model(m, new mongoose.Schema(models[m]));
+}
+
+module.exports = {
+    getModel: function(name){
+        return mongoose.model(name);
+    }
+};
--- project/server/user.js
+++ project/server/user.js
@@ -1,6 +1,15 @@
 const express = require('express');
-const Router = express.Router();
- 
+const Router  = express.Router();
+const model   = require('./model');
+const User    = model.getModel('user');
+
+// 用户列表
+Router.get('/list', function(req, res){
+    User.find({}, function(err, doc){
+        return res.json(doc);
+    });
+}); 
+
 Router.get('/info', function(req, res){
     return res.json({code: '1'});
 });
