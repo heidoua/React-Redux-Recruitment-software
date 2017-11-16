b9300275257ff891a20dd2c58fbd666d75f15ceb
FangFeiyue <18766131540@163.com>
2017-11-16 11:22:39 +0800

    解决刷新页面登录信息失效的问题

--- project/src/component/auth_route/AuthRote.js
+++ project/src/component/auth_route/AuthRote.js
@@ -14,14 +14,14 @@
          const publicList = ['/login', '/register']; 
          const pathname = this.props.location.pathname; 
 
-         if (publicList.indexOf(pathname)!= -1){
+         if (publicList.indexOf(pathname) > -1){
             return null ;
          }
 
         // 获取用户信息
         axios.get('/user/info').then(res => {
             if (res.status === 200){
-                if (res.data.code === '0'){
+                if (res.data.code === 0){
                     // 登录了
                     this.props.loadData(res.data.data);
                 }else{ // 没有登录
